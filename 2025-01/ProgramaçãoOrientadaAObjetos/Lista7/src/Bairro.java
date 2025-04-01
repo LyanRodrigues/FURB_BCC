deaffb1fa5e800f8a0ba39326e645f2e1fd83e7b
Thomas Leon Rodrigues <galanislyan@gmail.com>
2025-04-01 20:16:09 -0300

    Add files via upload

---
+++ 2025-01/ProgramaçãoOrientadaAObjetos/Lista7/src/Bairro.java
@@ -0,0 +1,18 @@
+public class Bairro {
+    private String nome;
+    private double coeficienteIptu;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getCoeficienteIptu() {
+        return coeficienteIptu;
+    }
+    public void setCoeficienteIptu(double coeficienteIptu) {
+        this.coeficienteIptu = coeficienteIptu;
+    }
+
+    
+}
---
+++ 2025-01/ProgramaçãoOrientadaAObjetos/Lista7/src/Finalidade.java
@@ -0,0 +1,3 @@
+public enum Finalidade {
+    RESIDENCIAL, COMERCIAL, INDUSTRIAL
+}
